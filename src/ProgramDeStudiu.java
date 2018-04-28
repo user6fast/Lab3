633d32df1a58a123c78ac67decbb05453171b29d
mac <mac@syn-PC.localdomain>
2018-04-28 13:18:24 +0300

    Initial commit

---
+++ src/ProgramDeStudiu.java
@@ -0,0 +1,11 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class ProgramDeStudiu {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("Hello Cluj");
+        System.out.println("Hello");
+    }
+}
+
